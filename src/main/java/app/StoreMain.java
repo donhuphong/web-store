b7b21386c42dd2253a6cfccf8d0774b93ccab3a1
donhuphong <donhuphong1902@gmail.com>
2020-11-14 01:00:13 +0700

    Revert "init login"

--- src/main/java/app/StoreMain.java
+++ src/main/java/app/StoreMain.java
@@ -1,11 +1,4 @@
 package app;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class StoreMain {
-    public static void main(String[] args) {
-        SpringApplication.run(StoreMain.class, args);
-    }
 }
