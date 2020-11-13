5d15d4184b2607eb07c0c8d3b85c728550e7a831
donhuphong <donhuphong1902@gmail.com>
2020-11-14 01:03:07 +0700

    commit revert

--- src/main/java/app/StoreMain.java
+++ src/main/java/app/StoreMain.java
@@ -1,4 +1,14 @@
 package app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StoreMain {
+    public StoreMain() {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreMain.class, args);
+    }
 }
