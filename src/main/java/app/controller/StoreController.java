101f4905c4e668098bdb4476fc45e03dcca936b5
donhuphong <donhuphong1902@gmail.com>
2020-11-14 00:39:38 +0700

    init login

--- src/main/java/app/controller/StoreController.java
+++ src/main/java/app/controller/StoreController.java
@@ -1,4 +1,13 @@
 package app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class StoreController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(){
+        return "hello";
+    }
 }
