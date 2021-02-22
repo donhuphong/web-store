b9bfe4cfae7b41bdc8a5c3b9e4052dccdd89ab96
donhuphong <donhuphong1902@gmail.com>
2021-02-22 23:58:20 +0700

    create fe

--- src/main/java/app/controller/AdminController.java
+++ src/main/java/app/controller/AdminController.java
@@ -1,34 +1,34 @@
-package app.controller;
-
-import app.reponse.UserInfo;
-import org.sonatype.inject.Parameters;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
-@RequestMapping(value = "/api/admin")
-public class AdminController {
-
-    @RequestMapping(value = "/create-user", method = RequestMethod.POST)
-    public String createUser() {
-        return "hello";
-    }
-
-    @RequestMapping(value = "/get-user", method = RequestMethod.GET)
-    public String getUser(@Parameters String user) {
-        UserInfo res = new UserInfo("phongchodien","phongchodien@gmail.com",
-                "Lươn Như Phong","+84964129341","male","06/12/1997");
-        return res.convertToGSon(res);
-    }
-
-    @RequestMapping(value = "/get-all-user", method = RequestMethod.GET)
-    public String getAllUser() {
-        return "listUser";
-    }
-
-
-}
+//package app.controller;
+//
+//import app.reponse.UserInfo;
+//import org.sonatype.inject.Parameters;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping(value = "/api/admin")
+//public class AdminController {
+//
+//    @RequestMapping(value = "/create-user", method = RequestMethod.POST)
+//    public String createUser() {
+//        return "hello";
+//    }
+//
+//    @RequestMapping(value = "/get-user", method = RequestMethod.GET)
+//    public String getUser(@Parameters String user) {
+//        UserInfo res = new UserInfo("phongchodien","phongchodien@gmail.com",
+//                "Lươn Như Phong","+84964129341","male","06/12/1997");
+//        return res.convertToGSon(res);
+//    }
+//
+//    @RequestMapping(value = "/get-all-user", method = RequestMethod.GET)
+//    public String getAllUser() {
+//        return "listUser";
+//    }
+//
+//
+//}
--- src/main/java/app/controller/StoreController.java
+++ src/main/java/app/controller/StoreController.java
@@ -1,22 +1,16 @@
 package app.controller;
 
-import app.common.ValidateUtils;
-import app.error.LoginError;
-import app.reponse.LoginResponse;
-import app.request.LoginRequest;
 import com.google.gson.Gson;
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import org.apache.http.HttpStatus;
-
-@RestController
+@Controller
 @RequestMapping(value = "/api")
 public class StoreController {
     public static Gson gson = new Gson();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
-        return "hello";
+        return "home";
     }
 }
